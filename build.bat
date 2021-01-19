813600e4ad9810d6e365b803d7d57c5dca228d7b
Wassim Kabbani <eng.wassim.kabbani@gmail.com>
2021-01-19 19:45:07 +0100

    change build file for win

--- build.bat
+++ build.bat
@@ -1,6 +1,13 @@
 call mvn clean package -Dmaven.test.skip=true
+
 call docker build -t payments .
-call docker-compose up -d --build
+
+call docker-compose up -d rabbitMq
+
 timeout /t 40
+
+docker-compose up -d payments
+
 call mvn test
+
 call docker-compose down
