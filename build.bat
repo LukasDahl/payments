6fa518fdc099733a77ef254e37d0afeda484d59b
Amtoft <lukas@amtoft.eu>
2021-01-19 19:24:35 +0100

    build.bat sleep reflects build.sh

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 call mvn clean package -Dmaven.test.skip=true
 call docker build -t payments .
 call docker-compose up -d --build
-timeout /t 10
+timeout /t 20
 call mvn test
 call docker-compose down
